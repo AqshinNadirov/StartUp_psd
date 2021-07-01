2d4b0fb49d20c72c293a167c8414ea0d177d2648
Aqshin Nadirov <nadirov.main@gmail.com>
2021-07-01 19:54:20 +0400

    js fav

--- js/scirpt.js
+++ js/scirpt.js
@@ -23,8 +23,8 @@ $(document).ready(function(){
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icon/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icon/right.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icon/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icon/right.svg"></button>',
         responsive: [
             {
               breakpoint: 768,
